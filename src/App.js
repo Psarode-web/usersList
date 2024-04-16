899e8edfc55b5726a1a2b057216c73b8b727503d
Psarode-web <sarode.piyus99@gmail.com>
2024-04-16 08:46:36 +0530

    Revert "testing one"
    This reverts commit e3f3102eefe03a1219e555ab944687617df92806.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <p>piyushfffff</p>
+      <p>piyush</p>
       <RouterProvider router={router} />
     </div>
   );
